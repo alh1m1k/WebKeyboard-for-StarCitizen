6f4a921520c1ffdba71425f0b6ca9bc54081b423
alhimik <alhimik.roman@gmail.com>
2026-02-26 21:10:11 +0300

    experimental locking mechanism

---
+++ main/syncing/critical_shared.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <atomic>
+
+#include "freertos/portmacro.h"
+#include "freertos/task.h"
+
+namespace syncing {
+
+/*
+ * This class c++ representation of freertos taskENTER_CRITICAL&taskEXIT_CRITICAL functions
+ * With difference that this barrier implements a shared_lock interface.
+ * It allows an arbitrary number of concurrent reads but synchronizes writes and writes with reads.
+ * The time spent in the lock should be minimal, no IO operations (the task was active when waiting)
+ * Use this class only to synchronize reading and writing inside ram memory.
+ * Incorrect use of the class may result in a device reboot.
+ */
+#warning "experimental critical_shared syncing in use"
+class critical_shared {
+
+	//this is reimplementation of freertos function taskENTER_CRITICAL ability to preserve
+	//interrupt state, there is no way to access that storage.
+	static inline BaseType_t port_uxInterruptState[portNUM_PROCESSORS] = {};
+
+	std::atomic<int> locker = 0;
+
+	public:
+		typedef std::atomic<int>& native_handle_type;
+
+		critical_shared() = default;
+		critical_shared( const critical_shared& ) = delete;
+
+		inline void lock() {
+			//no nested lock allowed inside c++, so it simplified.
+			port_uxInterruptState[xPortGetCoreID()] = portSET_INTERRUPT_MASK_FROM_ISR();
+			for (;;) {
+				int expected = 0;
+				if (locker.compare_exchange_weak(expected, -1, std::memory_order_acquire, std::memory_order_relaxed)) {
+					return;
+				}
+			}
+		}
+
+		bool try_lock() {
+			port_uxInterruptState[xPortGetCoreID()] = portSET_INTERRUPT_MASK_FROM_ISR();
+			int expected = 0;
+			if (locker.compare_exchange_weak(expected, -1, std::memory_order_acquire, std::memory_order_relaxed)) {
+				return true;
+			}
+			portCLEAR_INTERRUPT_MASK_FROM_ISR(
+				port_uxInterruptState[xPortGetCoreID()]
+			);
+			return false;
+		}
+
+		inline void unlock() {
+			locker.store(0, std::memory_order_release);
+			portCLEAR_INTERRUPT_MASK_FROM_ISR(
+				port_uxInterruptState[xPortGetCoreID()]
+			);
+		}
+
+		inline void lock_shared() {
+			port_uxInterruptState[xPortGetCoreID()] = portSET_INTERRUPT_MASK_FROM_ISR();
+			for (auto current = locker.load(std::memory_order_relaxed); true; current = locker.load(std::memory_order_relaxed)) {
+				if (current >= 0) {
+					if (locker.compare_exchange_weak(current, current+1, std::memory_order_acquire, std::memory_order_relaxed)) {
+						return;
+					}
+				}
+			}
+		}
+
+		bool try_lock_shared() {
+			port_uxInterruptState[xPortGetCoreID()] = portSET_INTERRUPT_MASK_FROM_ISR();
+			if (auto current = locker.load(std::memory_order_relaxed); current >= 0) {
+				if (locker.compare_exchange_weak(current, current+1, std::memory_order_acquire, std::memory_order_relaxed)) {
+					return true;
+				}
+			}
+			portCLEAR_INTERRUPT_MASK_FROM_ISR(
+				port_uxInterruptState[xPortGetCoreID()]
+			);
+			return false;
+		}
+
+		inline void unlock_shared() {
+			locker.fetch_sub(1, std::memory_order_release);
+			portCLEAR_INTERRUPT_MASK_FROM_ISR(
+				port_uxInterruptState[xPortGetCoreID()]
+			);
+		}
+
+		native_handle_type native_handle() {
+			return locker;
+		}
+};
+
+}
